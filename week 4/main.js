a23392936f5d9956a9fa346f99aad01cf7a46aba
dm2144 <dm2144@hw.ac.uk>
2023-11-09 12:49:37 +0400

    2

--- week 4/main.js
+++ week 4/main.js
@@ -1,7 +1,8 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://dm2144.github.io/WEBPRO/cities1.json');
+ourRequest.open('GET', 'https://dm2144.github.io/WEBPRO/week%204/cities1.json');
 ourRequest.onload = function() {
-console.log(ourRequest.responseText);
+var ourData = JSON.parse(ourRequest.responseText);
+console.log(ourData[0]);
 };
 ourRequest.send();
 
